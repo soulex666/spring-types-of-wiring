cb1078c92d50760feb74a38866c2522664b93192
Vladimir <soulexmail@gmail.com>
2021-03-24 18:06:00 +0300

    #1. Add dependencies logic

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Ocean1 {
 
-    @Autowired
     private Island2 island;
 
     @Override
     public String toString() {
         return "на океане остров " + island.toString();
+    }
+
+    @Autowired
+    public void setIsland(Island2 island) {
+        this.island = island;
     }
 }
